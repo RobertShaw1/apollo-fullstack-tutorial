aa16a50015390f103742a7d08b66a441304a9586
Robert Shaw <r.shaw1@yahoo.com>
2019-03-19 20:08:55 -0500

    server deployed

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,5 +1,6 @@
 /* Node Modules */
 const { ApolloServer } = require('apollo-server');
+const isEmail = require('isemail');
 
 /* Local Modules */
 const typeDefs = require('./schema');
@@ -12,6 +13,19 @@ const { createStore } = require('./utils');
 const store = createStore();
 
 const server = new ApolloServer({
+  context: async ({ req }) => {
+    // simple auth check on every request
+    const auth = (req.headers && req.headers.authorization) || '';
+    const email = Buffer.from(auth, 'base64').toString('ascii');
+
+    // if the email isn't formatted validly, return null for user
+    if (!isEmail.validate(email)) return { user: null };
+    // find a user by their email
+    const users = await store.users.findOrCreate({ where: { email } });
+    const user = users && users[0] ? users[0] : null;
+
+    return { user: { ...user.dataValues } };
+  },
   typeDefs,
   resolvers,
   dataSources: () => ({
