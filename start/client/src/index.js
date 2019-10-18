899f915c9c5f89b6b1657f064649425e31694bd3
Robert Shaw <r.shaw1@yahoo.com>
2019-10-17 21:29:39 -0500

    set up client cache

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -1,24 +1,51 @@
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
-import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloProvider, useQuery } from '@apollo/react-hooks';
 import ReactDOM from 'react-dom';
 import React from 'react';
+import gql from 'graphql-tag';
 
 import Pages from './pages';
+import Login from './pages/login';
+import injectStyles from './styles';
+import { resolvers, typeDefs } from './resolvers';
 
 const cache = new InMemoryCache();
-const link = new HttpLink({
-  uri: 'http://localhost:4000/'
-})
 
 const client = new ApolloClient({
   cache,
-  link,
   connectToDevTools: true,
-})
+  link: new HttpLink({
+    uri: 'http://localhost:4000/graphql',
+    headers: {
+      authorization: localStorage.getItem('token'),
+    },
+  }),
+  typeDefs,
+  resolvers,
+});
 
+cache.writeData({
+  data: {
+    isLoggedIn: !!localStorage.getItem('token'),
+    cartItems: [],
+  },
+});
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+injectStyles();
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Pages />
+    <IsLoggedIn />
   </ApolloProvider>, document.getElementById('root'));
